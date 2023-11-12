4edff04d84ed3bb8bf1fe26e3fcbbf3dc4e51e97
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-11-12 22:24:43 +0200

    method.routing_key - sms/email

--- consumer.py
+++ consumer.py
@@ -20,21 +20,21 @@ def consumer_contacts(connection: BlockingConnection, crud: MongoCRUD, exchange_
     channel = connection.channel()
     channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
 
-    def callback(ch, method, properties, body, send_key):
+    def callback(ch, method, properties, body):
         message = body.decode()
-        # send_key = "email_sent"  # TODO
-
+        send_key = queue_field.get(method.routing_key)
+        send_attr = f"{send_key}_sent"
         contact = crud.read(message)
         if contact is not None:
             print(f" [x] Send {send_key} to  {contact.full_name}")
-            crud.update_send_status(contact.id, send_key, True)
+            crud.update_send_status(contact.id, send_attr, True)
 
         print(f" [x] Completed {method.delivery_tag} task")
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     for queue, field in queue_field.items():
         channel.basic_qos(prefetch_count=1)
-        channel.basic_consume(queue=queue, on_message_callback=callback, arguments={'send_key': field})
+        channel.basic_consume(queue=queue, on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
